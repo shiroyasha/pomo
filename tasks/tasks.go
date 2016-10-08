ee27116948b0670bc0379fbe047a05be5a403a2f
Igor Šarčević <igor@renderedtext.com>
2016-10-08 23:32:47 +0200

    Read and save tasks to file

--- tasks/tasks.go
+++ tasks/tasks.go
@@ -1,8 +1,10 @@
 package tasks
 
 import (
+	"bufio"
 	"fmt"
 	"gopkg.in/urfave/cli.v1"
+	"os"
 	"strings"
 )
 
@@ -12,19 +14,46 @@ func Status(c *cli.Context) error {
 }
 
 func Add(c *cli.Context) error {
-	var task = allArgs(c)
+	task := allArgs(c)
 
 	if task == "" {
 		return cli.NewExitError("Task can't be empty.", 10)
 	}
 
-	fmt.Println(task)
+	file, err := os.OpenFile("/tmp/nesto", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
+
+	_, err = file.WriteString(task)
+	_, err = file.WriteString("\n")
+
+	if err != nil {
+		fmt.Println("ERROR:", err)
+
+		return cli.NewExitError("Failed to save the task", 11)
+	}
+
+	file.Close()
 
 	return nil
 }
 
 func List(c *cli.Context) error {
-	fmt.Println("list")
+	file, err := os.Open("/tmp/nesto")
+
+	defer file.Close()
+
+	if err != nil {
+		return cli.NewExitError("Could not load tasks.", 12)
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	index := 1
+	for scanner.Scan() {
+		fmt.Printf("%d) %s\n", index, scanner.Text())
+
+		index += 1
+	}
 
 	return nil
 }
