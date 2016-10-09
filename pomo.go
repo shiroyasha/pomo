52c27a2eb948cf61cee68fb4895ba59b0df54121
Igor Šarčević <igor@renderedtext.com>
2016-10-09 13:41:10 +0200

    Extract CLI from tasks namespace

--- pomo.go
+++ pomo.go
@@ -1,43 +1,85 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/shiroyasha/pomo/tasks"
 	"gopkg.in/urfave/cli.v1"
 )
+
+func status(c *cli.Context) error {
+	fmt.Println("status")
+	return nil
+}
+
+func add(c *cli.Context) error {
+	task := allArgs(c)
+
+	if task == "" {
+		return cli.NewExitError("Task can't be empty.", 10)
+	}
+
+	tasks.Add(task)
+
+	return nil
+}
+
+func list(c *cli.Context) error {
+	for index, task := range tasks.Load() {
+		fmt.Printf("%d) %s\n", index, task)
+	}
+
+	return nil
+}
+
+func remove(c *cli.Context) error {
+	index, _ := strconv.Atoi(c.Args().First())
+
+	tasks.Remove(index)
+
+	return nil
+}
+
+func allArgs(c *cli.Context) string {
+	var args = strings.Join(c.Args(), " ")
+
+	return strings.Trim(args, " ")
+}
 
 func main() {
 	app := cli.NewApp()
 	app.Name = "pomo"
 	app.Usage = "Pomodoro timer for the Command Line"
 	app.Version = "0.1.0"
-	app.Action = tasks.Status
+	app.Action = status
 
 	app.Commands = []cli.Command{
 		{
 			Name:    "add",
 			Aliases: []string{"a"},
 			Usage:   "Add a task to the list",
-			Action:  tasks.Add,
+			Action:  add,
 		},
 		{
 			Name:    "remove",
 			Aliases: []string{"rm"},
 			Usage:   "Remove a task from the list",
-			Action:  tasks.Remove,
+			Action:  remove,
 		},
 		{
 			Name:    "list",
 			Aliases: []string{"ls"},
 			Usage:   "List tasks",
-			Action:  tasks.List,
+			Action:  list,
 		},
 		{
 			Name:    "status",
 			Aliases: []string{"st"},
 			Usage:   "Displays status",
-			Action:  tasks.Status,
+			Action:  status,
 		},
 	}
 
--- tasks/tasks.go
+++ tasks/tasks.go
@@ -3,78 +3,33 @@ package tasks
 import (
 	"bufio"
 	"fmt"
-	"gopkg.in/urfave/cli.v1"
 	"os"
-	"strconv"
-	"strings"
 )
 
-func Status(c *cli.Context) error {
-	fmt.Println("status")
-	return nil
-}
+const filePath = "/tmp/tasks"
 
-func Add(c *cli.Context) error {
-	task := allArgs(c)
-
-	if task == "" {
-		return cli.NewExitError("Task can't be empty.", 10)
-	}
-
-	appendTask(task)
-
-	return nil
-}
-
-func List(c *cli.Context) error {
-	for index, task := range loadTasks() {
-		fmt.Printf("%d) %s\n", index, task)
-	}
-
-	return nil
-}
-
-func Remove(c *cli.Context) error {
-	index, _ := strconv.Atoi(c.Args().First())
-
-	removeTask(index)
-
-	return nil
-}
-
-func removeTask(index int) error {
-	tasks := loadTasks()
-
-	tasks = append(tasks[:index], tasks[index+1:]...)
-
-	saveTasks(tasks)
-
-	return nil
-}
-
-func appendTask(task string) {
-	tasks := loadTasks()
+func Add(task string) error {
+	tasks := Load()
 
 	tasks = append(tasks, task)
 
-	saveTasks(tasks)
+	Save(tasks)
+
+	return nil
 }
 
-func saveTasks(tasks []string) {
-	fd, _ := os.Create("/tmp/nesto")
-	defer fd.Close()
+func Remove(index int) error {
+	tasks := Load()
 
-	writer := bufio.NewWriter(fd)
+	tasks = append(tasks[:index], tasks[index+1:]...)
 
-	for _, task := range tasks {
-		fmt.Fprintf(writer, "%s\n", task)
-	}
+	Save(tasks)
 
-	writer.Flush()
+	return nil
 }
 
-func loadTasks() []string {
-	file, err := os.Open("/tmp/nesto")
+func Load() []string {
+	file, err := os.Open(filePath)
 
 	defer file.Close()
 
@@ -94,8 +49,15 @@ func loadTasks() []string {
 	return result
 }
 
-func allArgs(c *cli.Context) string {
-	var args = strings.Join(c.Args(), " ")
+func Save(tasks []string) {
+	fd, _ := os.Create(filePath)
+	defer fd.Close()
 
-	return strings.Trim(args, " ")
+	writer := bufio.NewWriter(fd)
+
+	for _, task := range tasks {
+		fmt.Fprintf(writer, "%s\n", task)
+	}
+
+	writer.Flush()
 }
